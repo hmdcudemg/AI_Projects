6f6c2e448ac07415bbbef0fbee477953af368b62
David Mendez Guardado <MendezDavidE@JohnDeere.com>
2018-08-01 17:09:10 -0500

    -Getting Started With Images.

--- OpenCV_Projects/GUIFeatures/GettingStartedWithImages/LoadImage.py
+++ OpenCV_Projects/GUIFeatures/GettingStartedWithImages/LoadImage.py
@@ -6,5 +6,4 @@ img = cv2.imread('img/watch.jpg', cv2.IMREAD_GRAYSCALE)
 
 plt.imshow(img, cmap='gray', interpolation='bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.plot([200, 300, 400], [100, 200, 300], 'c', linewidth=5)
 plt.show()
